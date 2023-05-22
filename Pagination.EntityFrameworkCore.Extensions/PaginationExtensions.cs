dc30cce1f0a1464703418bc8348ca07808567274
Welcome Sithole <sitholewb@gmail.com>
2023-05-22 11:48:54 +0200

    Replace PaginationAuto on PaginationExtensions

--- Pagination.EntityFrameworkCore.Extensions/PaginationExtensions.cs
+++ Pagination.EntityFrameworkCore.Extensions/PaginationExtensions.cs
@@ -5,70 +5,70 @@ using System.Linq.Expressions;
 
 namespace Pagination.EntityFrameworkCore.Extensions
 {
-	//Non Async Methods
-	public static partial class PaginationExtensions
-	{
-		public static Pagination<TSource> AsPagination<TSource>(this IQueryable<TSource> source, int page, int limit, string sortColumn = "", bool orderByDescending = false)
-		{
-			PaginationExtensionsHelper.ValidateInputs(page, limit);
+    //Non Async Methods
+    public static partial class PaginationExtensions
+    {
+        public static Pagination<TSource> AsPagination<TSource>(this IQueryable<TSource> source, int page, int limit, string sortColumn = "", bool orderByDescending = false)
+        {
+            PaginationExtensionsHelper.ValidateInputs(page, limit);
 
-			var totalItems = source.Count();
-			if (!string.IsNullOrEmpty(sortColumn))
-			{
-				source = orderByDescending ? source.OrderByDescending(p => EF.Property<object>(p, sortColumn)) : source.OrderBy(p => EF.Property<object>(p, sortColumn));
-			}
-			var results = source.Skip((page - 1) * limit).Take(limit);
+            var totalItems = source.Count();
+            if (!string.IsNullOrEmpty(sortColumn))
+            {
+                source = orderByDescending ? source.OrderByDescending(p => EF.Property<object>(p, sortColumn)) : source.OrderBy(p => EF.Property<object>(p, sortColumn));
+            }
+            var results = source.Skip((page - 1) * limit).Take(limit);
 
-			return new Pagination<TSource>(results.ToList(), totalItems, page, limit);
-		}
+            return new Pagination<TSource>(results.ToList(), totalItems, page, limit);
+        }
 
-		public static Pagination<TSource> AsPagination<TSource>(this IQueryable<TSource> source, int page, int limit, Expression<Func<TSource, bool>> expression, string sortColumn = "", bool orderByDescending = false)
-		{
-			PaginationExtensionsHelper.ValidateInputs(page, limit);
+        public static Pagination<TSource> AsPagination<TSource>(this IQueryable<TSource> source, int page, int limit, Expression<Func<TSource, bool>> expression, string sortColumn = "", bool orderByDescending = false)
+        {
+            PaginationExtensionsHelper.ValidateInputs(page, limit);
 
-			var totalItems = source.Where(expression).Count();
-			var results = Enumerable.Empty<TSource>();
-			if (!string.IsNullOrEmpty(sortColumn))
-			{
-				results = (orderByDescending ? source.Where(expression).OrderByDescending(p => EF.Property<object>(p, sortColumn)) : source.Where(expression).OrderBy(p => EF.Property<object>(p, sortColumn))).Skip((page - 1) * limit).Take(limit);
-			}
-			else
-			{
-				results = source.Where(expression).Skip((page - 1) * limit).Take(limit);
-			}
-			return new Pagination<TSource>(results, totalItems, page, limit);
-		}
+            var totalItems = source.Where(expression).Count();
+            var results = Enumerable.Empty<TSource>();
+            if (!string.IsNullOrEmpty(sortColumn))
+            {
+                results = (orderByDescending ? source.Where(expression).OrderByDescending(p => EF.Property<object>(p, sortColumn)) : source.Where(expression).OrderBy(p => EF.Property<object>(p, sortColumn))).Skip((page - 1) * limit).Take(limit);
+            }
+            else
+            {
+                results = source.Where(expression).Skip((page - 1) * limit).Take(limit);
+            }
+            return new Pagination<TSource>(results, totalItems, page, limit);
+        }
 
-		public static PaginationAuto<TSource, Tdestination> AsPagination<TSource, Tdestination>(this IQueryable<TSource> source, int page, int limit, Func<TSource, Tdestination> convertTsourceToTdestinationMethod, string sortColumn = "", bool orderByDescending = false)
-		{
-			PaginationExtensionsHelper.ValidateInputs(page, limit);
+        public static Pagination<Tdestination> AsPagination<TSource, Tdestination>(this IQueryable<TSource> source, int page, int limit, Func<TSource, Tdestination> convertTsourceToTdestinationMethod, string sortColumn = "", bool orderByDescending = false)
+        {
+            PaginationExtensionsHelper.ValidateInputs(page, limit);
 
-			var totalItems = source.Count();
-			if (!string.IsNullOrEmpty(sortColumn))
-			{
-				source = orderByDescending ? source.OrderByDescending(p => EF.Property<object>(p, sortColumn)) : source.OrderBy(p => EF.Property<object>(p, sortColumn));
-			}
+            var totalItems = source.Count();
+            if (!string.IsNullOrEmpty(sortColumn))
+            {
+                source = orderByDescending ? source.OrderByDescending(p => EF.Property<object>(p, sortColumn)) : source.OrderBy(p => EF.Property<object>(p, sortColumn));
+            }
 
-			var results = source.Skip((page - 1) * limit).Take(limit);
+            var results = source.Skip((page - 1) * limit).Take(limit);
 
-			return new PaginationAuto<TSource, Tdestination>(results, totalItems, convertTsourceToTdestinationMethod, page, limit);
-		}
+            return new Pagination<Tdestination>(results?.Select(a => convertTsourceToTdestinationMethod(a)) ?? Enumerable.Empty<Tdestination>(), totalItems, page, limit);
+        }
 
-		public static PaginationAuto<TSource, Tdestination> AsPagination<TSource, Tdestination>(this IQueryable<TSource> source, int page, int limit, Expression<Func<TSource, bool>> expression, Func<TSource, Tdestination> convertTsourceToTdestinationMethod, string sortColumn = "", bool orderByDescending = false)
-		{
-			PaginationExtensionsHelper.ValidateInputs(page, limit);
+        public static Pagination<Tdestination> AsPagination<TSource, Tdestination>(this IQueryable<TSource> source, int page, int limit, Expression<Func<TSource, bool>> expression, Func<TSource, Tdestination> convertTsourceToTdestinationMethod, string sortColumn = "", bool orderByDescending = false)
+        {
+            PaginationExtensionsHelper.ValidateInputs(page, limit);
 
-			var totalItems = source.Where(expression).Count();
-			var results = Enumerable.Empty<TSource>();
-			if (!string.IsNullOrEmpty(sortColumn))
-			{
-				results = (orderByDescending ? source.Where(expression).OrderByDescending(p => EF.Property<object>(p, sortColumn)) : source.Where(expression).OrderBy(p => EF.Property<object>(p, sortColumn))).Skip((page - 1) * limit).Take(limit);
-			}
-			else
-			{
-				results = source.Where(expression).Skip((page - 1) * limit).Take(limit);
-			}
-			return new PaginationAuto<TSource, Tdestination>(results, totalItems, convertTsourceToTdestinationMethod, page, limit);
-		}
-	}
+            var totalItems = source.Where(expression).Count();
+            var results = Enumerable.Empty<TSource>();
+            if (!string.IsNullOrEmpty(sortColumn))
+            {
+                results = (orderByDescending ? source.Where(expression).OrderByDescending(p => EF.Property<object>(p, sortColumn)) : source.Where(expression).OrderBy(p => EF.Property<object>(p, sortColumn))).Skip((page - 1) * limit).Take(limit);
+            }
+            else
+            {
+                results = source.Where(expression).Skip((page - 1) * limit).Take(limit);
+            }
+            return new Pagination<Tdestination>(results?.Select(a => convertTsourceToTdestinationMethod(a)) ?? Enumerable.Empty<Tdestination>(), totalItems, page, limit);
+        }
+    }
 }
