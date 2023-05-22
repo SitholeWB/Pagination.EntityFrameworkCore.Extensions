8df7c33a32b36abfe18178c6bb65f1613be2dd4e
Welcome Sithole <sitholewb@gmail.com>
2023-05-22 19:59:29 +0200

    add example ToDtoAsync

--- Examples/BlazorApp.SQLite/Helpers/CountryHelper.cs
+++ Examples/BlazorApp.SQLite/Helpers/CountryHelper.cs
@@ -3,17 +3,30 @@ using BlazorApp.SQLite.Entities;
 
 namespace BlazorApp.SQLite.Helpers
 {
-	public class CountryHelper
-	{
-		public static CountryDto ToDto(Country country)
-		{
-			return new CountryDto
-			{
-				DateAdded = country.DateAdded,
-				Id = country.Id,
-				LastModifiedDate = country.LastModifiedDate,
-				Name = country.Name,
-			};
-		}
-	}
+    public class CountryHelper
+    {
+        public static CountryDto ToDto(Country country)
+        {
+            return new CountryDto
+            {
+                DateAdded = country.DateAdded,
+                Id = country.Id,
+                LastModifiedDate = country.LastModifiedDate,
+                Name = country.Name,
+            };
+        }
+
+        public static async Task<CountryDto> ToDtoAsync(Country country)
+        {
+            var seconds = new Random().Next(0, 2);
+            await Task.Delay(TimeSpan.FromSeconds(seconds));
+            return new CountryDto
+            {
+                DateAdded = country.DateAdded,
+                Id = country.Id,
+                LastModifiedDate = country.LastModifiedDate,
+                Name = country.Name,
+            };
+        }
+    }
 }
