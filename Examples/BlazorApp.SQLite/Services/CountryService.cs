ea5594e54bec57daea880a1ed75ed3820c4d435d
Welcome Sithole <sitholewb@gmail.com>
2023-05-22 19:52:31 +0200

    Upgrade nuget packages on examples

--- Examples/BlazorApp.SQLite/Services/CountryService.cs
+++ Examples/BlazorApp.SQLite/Services/CountryService.cs
@@ -8,71 +8,71 @@ using Pagination.EntityFrameworkCore.Extensions;
 
 namespace BlazorApp.SQLite.Services
 {
-	public class CountryService
-	{
-		private readonly BlazorAppSQLiteContext _context;
+    public class CountryService
+    {
+        private readonly BlazorAppSQLiteContext _context;
 
-		public CountryService(BlazorAppSQLiteContext context)
-		{
-			_context = context;
-		}
+        public CountryService(BlazorAppSQLiteContext context)
+        {
+            _context = context;
+        }
 
-		public async Task<Country> AddCountryAsync(AddCountryModel country)
-		{
-			if (string.IsNullOrWhiteSpace(country?.Name))
-			{
-				throw new ArgumentException("Country name is required.");
-			}
+        public async Task<Country> AddCountryAsync(AddCountryModel country)
+        {
+            if (string.IsNullOrWhiteSpace(country?.Name))
+            {
+                throw new ArgumentException("Country name is required.");
+            }
 
-			var entity = _context.Country.Add(new Country
-			{
-				Name = country.Name,
-				DateAdded = DateTimeOffset.UtcNow,
-				Id = Guid.NewGuid(),
-				LastModifiedDate = DateTimeOffset.UtcNow,
-			});
-			await _context.SaveChangesAsync();
-			return entity.Entity;
-		}
+            var entity = _context.Country.Add(new Country
+            {
+                Name = country.Name,
+                DateAdded = DateTimeOffset.UtcNow,
+                Id = Guid.NewGuid(),
+                LastModifiedDate = DateTimeOffset.UtcNow,
+            });
+            await _context.SaveChangesAsync();
+            return entity.Entity;
+        }
 
-		public async Task<Country> UpdateCountryAsync(Guid id, UpdateCountryModel country)
-		{
-			if (string.IsNullOrWhiteSpace(country?.Name))
-			{
-				throw new ArgumentException("Country name is required.");
-			}
+        public async Task<Country> UpdateCountryAsync(Guid id, UpdateCountryModel country)
+        {
+            if (string.IsNullOrWhiteSpace(country?.Name))
+            {
+                throw new ArgumentException("Country name is required.");
+            }
 
-			var entity = await _context.Country.FindAsync(id);
-			if (entity == null)
-			{
-				throw new ArgumentException("Id not found.");
-			}
-			entity.Name = country.Name;
-			entity.LastModifiedDate = DateTimeOffset.UtcNow;
-			_context.Update(entity);
-			await _context.SaveChangesAsync();
-			return entity;
-		}
+            var entity = await _context.Country.FindAsync(id);
+            if (entity == null)
+            {
+                throw new ArgumentException("Id not found.");
+            }
+            entity.Name = country.Name;
+            entity.LastModifiedDate = DateTimeOffset.UtcNow;
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
 
-		public async Task DeleteCountryAsync(Guid id)
-		{
-			var entity = await _context.Country.FindAsync(id);
-			if (entity == null)
-			{
-				throw new ArgumentException("Id not found.");
-			}
-			_context.Remove(entity);
-			await _context.SaveChangesAsync();
-		}
+        public async Task DeleteCountryAsync(Guid id)
+        {
+            var entity = await _context.Country.FindAsync(id);
+            if (entity == null)
+            {
+                throw new ArgumentException("Id not found.");
+            }
+            _context.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
 
-		public async Task<PaginationAuto<Country, CountryDto>> GetCountries(int page = 1, int limit = 10)
-		{
-			return await _context.Country.AsPaginationAsync<Country, CountryDto>(page, limit, CountryHelper.ToDto);
-		}
+        public async Task<Pagination<CountryDto>> GetCountries(int page = 1, int limit = 10)
+        {
+            return await _context.Country.AsPaginationAsync<Country, CountryDto>(page, limit, CountryHelper.ToDto);
+        }
 
-		public async Task<Country?> GetCountry(Guid id)
-		{
-			return await _context.Country.FirstOrDefaultAsync(x => x.Id == id);
-		}
-	}
+        public async Task<Country?> GetCountry(Guid id)
+        {
+            return await _context.Country.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
 }
--- Examples/WebApi/Controllers/CountriesController.cs
+++ Examples/WebApi/Controllers/CountriesController.cs
@@ -8,111 +8,111 @@ using WebApi.Models;
 
 namespace WebApi.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class CountriesController : ControllerBase
-	{
-		private readonly WebApiContext _context;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly WebApiContext _context;
 
-		public CountriesController(WebApiContext context)
-		{
-			_context = context;
-		}
+        public CountriesController(WebApiContext context)
+        {
+            _context = context;
+        }
 
-		// GET: api/Countries
-		[HttpGet]
-		public async Task<ActionResult<PaginationAuto<Country, CountryDto>>> GetCountries(int page = 1, int limit = 10)
-		{
-			if (_context.Country == null)
-			{
-				return NotFound();
-			}
-			return await _context.Country.AsPaginationAsync<Country, CountryDto>(page, limit, CountryHelper.ToDto);
-		}
+        // GET: api/Countries
+        [HttpGet]
+        public async Task<ActionResult<Pagination<CountryDto>>> GetCountries(int page = 1, int limit = 10)
+        {
+            if (_context.Country == null)
+            {
+                return NotFound();
+            }
+            return await _context.Country.AsPaginationAsync<Country, CountryDto>(page, limit, CountryHelper.ToDto);
+        }
 
-		// GET: api/Countries/5
-		[HttpGet("{id}")]
-		public async Task<ActionResult<Country>> GetCountry(Guid id)
-		{
-			if (_context.Country == null)
-			{
-				return NotFound();
-			}
-			var country = await _context.Country.FindAsync(id);
+        // GET: api/Countries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(Guid id)
+        {
+            if (_context.Country == null)
+            {
+                return NotFound();
+            }
+            var country = await _context.Country.FindAsync(id);
 
-			if (country == null)
-			{
-				return NotFound();
-			}
+            if (country == null)
+            {
+                return NotFound();
+            }
 
-			return country;
-		}
+            return country;
+        }
 
-		// PUT: api/Countries/5 To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-		[HttpPut("{id}")]
-		public async Task<IActionResult> PutCountry(Guid id, UpdateCountryModel country)
-		{
-			if (string.IsNullOrWhiteSpace(country?.Name))
-			{
-				return BadRequest("Country name is required.");
-			}
-			var entity = await _context.Country.FindAsync(id);
-			if (entity == null)
-			{
-				return NotFound("Id not found.");
-			}
-			entity.Name = country.Name;
-			entity.LastModifiedDate = DateTimeOffset.UtcNow;
-			_context.Update(entity);
+        // PUT: api/Countries/5 To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(Guid id, UpdateCountryModel country)
+        {
+            if (string.IsNullOrWhiteSpace(country?.Name))
+            {
+                return BadRequest("Country name is required.");
+            }
+            var entity = await _context.Country.FindAsync(id);
+            if (entity == null)
+            {
+                return NotFound("Id not found.");
+            }
+            entity.Name = country.Name;
+            entity.LastModifiedDate = DateTimeOffset.UtcNow;
+            _context.Update(entity);
 
-			await _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
 
-			return NoContent();
-		}
+            return NoContent();
+        }
 
-		// POST: api/Countries To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-		[HttpPost]
-		public async Task<ActionResult<Country>> PostCountry(AddCountryModel country)
-		{
-			if (string.IsNullOrWhiteSpace(country?.Name))
-			{
-				return BadRequest("Country name is required.");
-			}
+        // POST: api/Countries To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(AddCountryModel country)
+        {
+            if (string.IsNullOrWhiteSpace(country?.Name))
+            {
+                return BadRequest("Country name is required.");
+            }
 
-			if (_context.Country == null)
-			{
-				return Problem("Entity set 'WebApiContext.Country'  is null.");
-			}
-			var entity = _context.Country.Add(new Country
-			{
-				Name = country.Name,
-				DateAdded = DateTimeOffset.UtcNow,
-				Id = Guid.NewGuid(),
-				LastModifiedDate = DateTimeOffset.UtcNow,
-			});
-			await _context.SaveChangesAsync();
+            if (_context.Country == null)
+            {
+                return Problem("Entity set 'WebApiContext.Country'  is null.");
+            }
+            var entity = _context.Country.Add(new Country
+            {
+                Name = country.Name,
+                DateAdded = DateTimeOffset.UtcNow,
+                Id = Guid.NewGuid(),
+                LastModifiedDate = DateTimeOffset.UtcNow,
+            });
+            await _context.SaveChangesAsync();
 
-			return CreatedAtAction("GetCountry", new { id = entity.Entity.Id }, entity.Entity);
-		}
+            return CreatedAtAction("GetCountry", new { id = entity.Entity.Id }, entity.Entity);
+        }
 
-		// DELETE: api/Countries/5
-		[HttpDelete("{id}")]
-		public async Task<IActionResult> DeleteCountry(Guid id)
-		{
-			if (_context.Country == null)
-			{
-				return NotFound();
-			}
-			var country = await _context.Country.FindAsync(id);
-			if (country == null)
-			{
-				return NotFound();
-			}
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(Guid id)
+        {
+            if (_context.Country == null)
+            {
+                return NotFound();
+            }
+            var country = await _context.Country.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
 
-			_context.Country.Remove(country);
-			await _context.SaveChangesAsync();
+            _context.Country.Remove(country);
+            await _context.SaveChangesAsync();
 
-			return NoContent();
-		}
-	}
+            return NoContent();
+        }
+    }
 }
