d7a04c3f95ae9e8ad136a192bdff9785aa854bc9
Welcome Sithole <sitholewb@gmail.com>
2023-05-23 19:02:29 +0200

    update Obsolete warning message

--- Pagination.EntityFrameworkCore.Extensions/PaginationAuto.cs
+++ Pagination.EntityFrameworkCore.Extensions/PaginationAuto.cs
@@ -4,17 +4,17 @@ using System.Linq;
 
 namespace Pagination.EntityFrameworkCore.Extensions
 {
-    [Obsolete("Simplify your code by using Pagination")]
-    public class PaginationAuto<Tsource, TDestination> : Pagination<TDestination>
+    [Obsolete("Use static methods Pagination.GetPagination(...) or Pagination.GetPaginationAsync(...)")]
+    public class PaginationAuto<TSource, TDestination> : Pagination<TDestination>
     {
-        [Obsolete("Simplify your code by using Pagination")]
-        public PaginationAuto(Pagination<Tsource> pagination, Func<Tsource, TDestination> convertTSourceToTDestinationMethod)
+        [Obsolete("Use static methods Pagination.GetPagination(...) or Pagination.GetPaginationAsync(...)")]
+        public PaginationAuto(Pagination<TSource> pagination, Func<TSource, TDestination> convertTSourceToTDestinationMethod)
         : base(pagination.Results?.Select(x => convertTSourceToTDestinationMethod(x)) ?? new List<TDestination>(), pagination.TotalItems, pagination.CurrentPage, (int)pagination.TotalItems, false)
         {
         }
 
-        [Obsolete("Simplify your code by using Pagination")]
-        public PaginationAuto(IEnumerable<Tsource> results, long totalItems, Func<Tsource, TDestination> convertTSourceToTDestinationMethod, int page = 1, int limit = 10, bool applyPageAndLimitToResults = false)
+        [Obsolete("Use static methods Pagination.GetPagination(...) or Pagination.GetPaginationAsync(...)")]
+        public PaginationAuto(IEnumerable<TSource> results, long totalItems, Func<TSource, TDestination> convertTSourceToTDestinationMethod, int page = 1, int limit = 10, bool applyPageAndLimitToResults = false)
        : base(results?.Select(x => convertTSourceToTDestinationMethod(x)) ?? new List<TDestination>(), totalItems, page, limit, applyPageAndLimitToResults)
         {
         }
