c942e2109717076b97ff96826325372948629eb0
Welcome Sithole <sitholewb@gmail.com>
2024-03-28 12:06:14 +0200

    Add default constructor

--- Pagination.EntityFrameworkCore.Extensions/Pagination.cs
+++ Pagination.EntityFrameworkCore.Extensions/Pagination.cs
@@ -5,68 +5,78 @@ using System.Threading.Tasks;
 
 namespace Pagination.EntityFrameworkCore.Extensions
 {
-	public class Pagination<T>
-	{
-		public long TotalItems { get; private set; }
-		public int CurrentPage { get; private set; }
-		public int? NextPage { get; private set; }
-		public int? PreviousPage { get; private set; }
-		public int TotalPages { get; private set; }
-		public IEnumerable<T> Results { get; private set; }
+    public class Pagination<T>
+    {
+        public long TotalItems { get; private set; }
+        public int CurrentPage { get; private set; }
+        public int? NextPage { get; private set; }
+        public int? PreviousPage { get; private set; }
+        public int TotalPages { get; private set; }
+        public IEnumerable<T> Results { get; private set; }
 
-		public Pagination(Pagination<T> pagination)
-		{
-			TotalItems = pagination.TotalPages;
-			CurrentPage = pagination.CurrentPage;
-			NextPage = pagination.NextPage;
-			PreviousPage = pagination.PreviousPage;
-			TotalPages = pagination.TotalPages;
-			Results = pagination.Results;
-		}
+        public Pagination()
+        {
+            TotalItems = 0;
+            CurrentPage = 1;
+            NextPage = null;
+            PreviousPage = null;
+            TotalPages = 1;
+            Results = Enumerable.Empty<T>();
+        }
 
-		public Pagination(IEnumerable<T> results, long totalItems, int page = 1, int limit = 10, bool applyPageAndLimitToResults = false)
-		{
-			if (page <= 0)
-			{
-				throw new PaginationException("Page must be greater than 0");
-			}
+        public Pagination(Pagination<T> pagination)
+        {
+            TotalItems = pagination.TotalPages;
+            CurrentPage = pagination.CurrentPage;
+            NextPage = pagination.NextPage;
+            PreviousPage = pagination.PreviousPage;
+            TotalPages = pagination.TotalPages;
+            Results = pagination.Results;
+        }
 
-			var startIndex = (page - 1) * limit;
-			var endIndex = page * limit;
+        public Pagination(IEnumerable<T> results, long totalItems, int page = 1, int limit = 10, bool applyPageAndLimitToResults = false)
+        {
+            if (page <= 0)
+            {
+                throw new PaginationException("Page must be greater than 0");
+            }
 
-			TotalItems = totalItems;
-			CurrentPage = page;
-			Results = results ?? Enumerable.Empty<T>();
-			if (applyPageAndLimitToResults)
-			{
-				Results = Results.Skip(startIndex).Take(limit);
-			}
-			if (startIndex > 0)
-			{
-				PreviousPage = page - 1;
-			}
-			if (endIndex < totalItems)
-			{
-				NextPage = page + 1;
-			}
+            var startIndex = (page - 1) * limit;
+            var endIndex = page * limit;
 
-			TotalPages = limit > 0 ? (int)Math.Ceiling((decimal)totalItems / (decimal)limit) : 0;
-		}
+            TotalItems = totalItems;
+            CurrentPage = page;
+            Results = results ?? Enumerable.Empty<T>();
+            if (applyPageAndLimitToResults)
+            {
+                Results = Results.Skip(startIndex).Take(limit);
+            }
+            if (startIndex > 0)
+            {
+                PreviousPage = page - 1;
+            }
+            if (endIndex < totalItems)
+            {
+                NextPage = page + 1;
+            }
 
-		public static Pagination<TDestination> GetPagination<TSource, TDestination>(IEnumerable<TSource> results, long totalItems, Func<TSource, TDestination> convertTSourceToTDestinationMethod, int page = 1, int limit = 10, bool applyPageAndLimitToResults = false)
-		{
-			var destinationResults = results?.Select(x => convertTSourceToTDestinationMethod(x)) ?? new List<TDestination>();
-			return new Pagination<TDestination>(destinationResults, totalItems, page, limit, applyPageAndLimitToResults);
-		}
+            TotalPages = limit > 0 ? (int)Math.Ceiling((decimal)totalItems / (decimal)limit) : 0;
+        }
 
-		public static async Task<Pagination<TDestination>> GetPaginationAsync<TSource, TDestination>(IEnumerable<TSource> results, long totalItems, Func<TSource, Task<TDestination>> convertTSourceToTDestinationMethod, int page = 1, int limit = 10, bool applyPageAndLimitToResults = false)
-		{
-			if (results == null)
-			{
-				return new Pagination<TDestination>(new List<TDestination>(), totalItems, page, limit, applyPageAndLimitToResults);
-			}
-			var destinationResults = await results.Select(async ev => await convertTSourceToTDestinationMethod(ev)).WhenAll().ConfigureAwait(false);
-			return new Pagination<TDestination>(destinationResults, totalItems, page, limit, applyPageAndLimitToResults);
-		}
-	}
+        public static Pagination<TDestination> GetPagination<TSource, TDestination>(IEnumerable<TSource> results, long totalItems, Func<TSource, TDestination> convertTSourceToTDestinationMethod, int page = 1, int limit = 10, bool applyPageAndLimitToResults = false)
+        {
+            var destinationResults = results?.Select(x => convertTSourceToTDestinationMethod(x)) ?? new List<TDestination>();
+            return new Pagination<TDestination>(destinationResults, totalItems, page, limit, applyPageAndLimitToResults);
+        }
+
+        public static async Task<Pagination<TDestination>> GetPaginationAsync<TSource, TDestination>(IEnumerable<TSource> results, long totalItems, Func<TSource, Task<TDestination>> convertTSourceToTDestinationMethod, int page = 1, int limit = 10, bool applyPageAndLimitToResults = false)
+        {
+            if (results == null)
+            {
+                return new Pagination<TDestination>(new List<TDestination>(), totalItems, page, limit, applyPageAndLimitToResults);
+            }
+            var destinationResults = await results.Select(async ev => await convertTSourceToTDestinationMethod(ev)).WhenAll().ConfigureAwait(false);
+            return new Pagination<TDestination>(destinationResults, totalItems, page, limit, applyPageAndLimitToResults);
+        }
+    }
 }
